c606fa6b830b1f69e22e0a58c646496f27799e5f
Ashok812 <chhetriarash81@gmail.com>
2021-06-09 12:05:40 +0545

    Error Fixing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import SearchBar from './components/SearchBar';
-import useFetch from './components/useFetch';
 
 function App() {
-  const api = "429736441cf3572838aa10530929f7cd"
 
-  const {data:wheaters, isPending, error } = useFetch(`http://api.openweathermap.org/data/2.5/weather?q=pokhara,Nepal&appid=${api}`)
   return (
     <div className="App">
       <h3>Weather BroadCasting</h3>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './search.css'
 import Show from './Show'
+
 function SearchBar() {
+    const [country, setCountry] = useState();
+    const [place, setPlace] = useState();
+    
+    const  handle = async() => {
+        const api = "429736441cf3572838aa10530929f7cd"
+        const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place},${country}&appid=${api}`).then(response => response.json()).then((data) => {
+            console.log(data)
+        })
+    }
+
+    
+
     return (
         <div>
             <div className="search">
-                <input type="text" placeholder="Search.." />
-                <button>Search Location</button>
+                <input type="text" placeholder="Country" value={country}
+                    onChange={(event) => setCountry(event.target.value)}
+                />
+                <input type="text" placeholder="Place" value={place}
+                    onChange={(event) => setPlace(event.target.value)}
+                />
+                <button onClick={handle}>Search Location</button>
             </div>
             <Show />
         </div>
